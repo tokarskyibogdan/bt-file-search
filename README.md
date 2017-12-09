3d08a944e3f5cd3c11635c6550600bbc2655e920
bogdan <tokarskyi.work@gmail.com>
2017-12-09 10:12:53 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# node-file-search
-Node JS async file search
+# bt-file-search
+Node JS async file search.
+
+## Installation
+
+``$ [sudo] npm install -g bt-file-search``
+
+## Usage:
+    
+##### CLI   
+
+    Usage:
+        bt-file-search [OPTION]=[ARG]
+    
+    Options: 
+        --DIR (required) base lookup directory
+        --TYPE (optional) [D|F] D - directory, F - file
+        --PATTERN (optional) regular expression to test file/directory name
+        --MIN-SIZE (optional) minimum file size [B|K|M|G], skipped for directories
+        --MAX-SIZE (optional) maximum file size [B|K|M|G], skipped for directories 
+        
+    (B - bytes, K - kilobytes, M - megabytes, G - gigabytes)    
+    
+##### Example:  
+
+> The order of the parameters is NOT strict. 
+
+``bt-file-search --DIR=./ --TYPE=F --MIN-SIZE=100M --MAX-SIZE=1G --PATTERN=\.js`` 
+
+### License and Copyright
+This software is released under the terms of the [ISC license](https://github.com/tokarskyibogdan/bt-file-search/blob/master/LICENSE.md).
---
+++ index.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const path = require('path');
+
+const args = process.argv.slice(2).reduce((acc, arg) => {
+    let [k, v = true] = arg.split('=');
+    acc[k.slice(2).toLowerCase().replace('-s', 'S')] = v;
+    return acc
+}, {});
+
+let directory = args.dir,
+    type = args.type,
+    pattern = args.pattern,
+    minSize = args.minSize,
+    maxSize = args.maxSize;
+
+if (!directory) throw new Error('--DIR is required param');
+
+const typeCheck = (type, file) => {
+    if (type === 'D') {
+        return file.isDirectory
+    } else if (type === 'F') {
+        return !file.isDirectory
+    } else {
+        return true;
+    }
+};
+
+const convertSize = size => {
+    let sizeType = size.slice(-1);
+    let sizeValue = size.substring(0, size.length - 1);
+    switch (sizeType) {
+        case 'K':
+            return sizeValue * 1024;
+        case 'M':
+            return sizeValue * 1048576;
+        case 'G':
+            return sizeValue * 1073741824;
+        default:
+            return sizeValue
+    }
+};
+
+let maxSizeCheck = (fileSize, maxSize) => {
+    if (fileSize && maxSize) {
+        return convertSize(maxSize) > fileSize;
+    } else {
+        return true;
+    }
+};
+
+let minSizeCheck = (fileSize, minSize) => {
+    if (fileSize && minSize) {
+        return convertSize(minSize) < fileSize;
+    } else {
+        return true;
+    }
+};
+
+let search = (directory, done) => {
+    let results = [];
+    fs.readdir(directory, (err, list) => {
+        if (err) return done(err);
+        let pending = list.length;
+        if (!pending) return done(null, results);
+        list.forEach(filePath => {
+            let fileName = filePath;
+            filePath = path.resolve(directory, filePath);
+            fs.stat(filePath, (err, stat) => {
+                if (stat && stat.isDirectory()) {
+                    search(filePath, (err, res) => {
+                        results.push({name: fileName, path: filePath, isDirectory: stat.isDirectory()});
+                        results = results.concat(res);
+                        if (!--pending) done(null, results);
+                    });
+                } else {
+                    results.push({name: fileName, path: filePath, size: stat.size, isDirectory: stat.isDirectory()});
+                    if (!--pending) done(null, results);
+                }
+            });
+        });
+    });
+};
+
+search(directory, function (err, results) {
+    if (err) throw err;
+    console.log(results.filter(file => file.name.match(pattern)
+        && typeCheck(type, file) && minSizeCheck(file.size, minSize)
+        && maxSizeCheck(file.size, maxSize)).map(file => file.path));
+});
